18a7ecfd2c16b81208f94a0c6f3c5ee26216c3b5
andrey lazar <andreylazarmr@gmail.com>
2023-11-11 17:22:36 +0300

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ToyStore store = new ToyStore();
+
+        store.addToy(new Toy("qwe", 1, 46));
+        store.addToy(new Toy("rty", 3, 17));
+        store.addToy(new Toy("aad", 2, 64));
+        store.addToy(new Toy("zxc", 3, 27));
+        store.addToy(new Toy("cvb", 1, 32));
+        store.addToy(new Toy("fds", 1, 44));
+        store.addToy(new Toy("hgf", 2, 49));
+
+        for (int i = 0; i < 9; i++) {
+            playToy(store);
+            System.out.println(store);
+        }
+
+
     }
+
+    static ToyStore playToy(ToyStore store) {
+        Toy prizeToy = store.play();
+        if (prizeToy == null) {
+            System.out.println("Все призы разыграны!");
+        } else {
+            System.out.println("Вы выиграли: " + prizeToy.getName());
+            List prizeToys = store.getPrizeToys();
+            prizeToys.add(prizeToy);
+        }
+        return store;
+    }
+
 }
